89000d54431c6e0f028e863d7a1540f1aea9954f
Hakim Boukerdoun <116846705+Iamhakim@users.noreply.github.com>
2025-01-03 23:33:02 +0100

    creating some functionalities

--- script.js
+++ script.js
@@ -4,8 +4,46 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+//play and pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+//update play&pause icon
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+
+//update progress & timestamp
+function updateProgress() {
+  return true;
+}
+
+//update setvideo to progress
+function setVideoProgress() {
+  return true;
+}
+
+//update stop video
+function stopVideo() {
+  return true;
+}
+
 //event listeners
 video.addEventListener('click', toggleVideoStatus);
-video.addEventListener('pause', updatePlayIcone);
-video.addEventListener('play', updatePlayIcone);
-video.addEventListener('timeupdate', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+
+stop.addEventListener('click', stopVideo);
+progress.addEventListener('change', setVideoProgress);
